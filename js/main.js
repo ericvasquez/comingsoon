0d9e888b599f499952dab8a333a723261607d966
Eric <eric.vasquez3327@gmail.com>
2023-03-10 10:18:04 -0600

    changed timer and updated phone

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const countdown = document.querySelector("body > section > div > div.countdown");
 
 // Set Launch Date To 1 Mar 2021 at 1300
-const launchDate = new Date('Dec 20, 2021 13:00:00').getTime();
+const launchDate = new Date('Apr 10, 2023 13:00:00').getTime();
 
 //Update every second
 const intvl = setInterval(() => {
